e6fef459884daf7b195e8349409df18d29689fc5
Deepti Nagarkar <deepti.nagarkar@gmail.com>
2011-05-12 13:54:28 +0000

    Constants Class for the Notes Connector

---
+++ source/java/com/google/enterprise/connector/notes/LotusConstants.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.google.enterprise.connector.notes;
+
+/**
+ * @author deepti_nagarkar
+ * May 4, 2011
+ * 2011
+ */
+public final class LotusConstants {
+
+	//DOC Stub Item Names
+	public static final String CS_DATABASE="CS_OriginalDatabase";
+	public static final String CS_SEQUENCEID="CS_SequenceID";
+	public static final String CS_ORIGDOCID = "CS_OriginalDocUnid";
+	public static final String CS_INDEXSERVER ="CS_IndexServer";
+	public static final String CS_STATUS="CS_Status";
+	//Document States in the Traversal Q 
+	public static final String STATUS_INCRAWL="InCrawl";
+	public static final String STATUS_NEW="New";
+	public static final String STATUS_FETCHED="Fetched";
+}
