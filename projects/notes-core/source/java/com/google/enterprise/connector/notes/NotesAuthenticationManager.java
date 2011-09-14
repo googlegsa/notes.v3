6a488fd4279440cf2665c8b0bb5ef82d307a5304
Gennis Emerson <gennis.d.emerson@gmail.com>
2011-09-14 17:55:41 +0000

    Coding standards and connector project conventions changes:
    Modified source to standard indentation (2 spaces for statements;
    4 for continued lines). Removed tabs. Added braces where
    required. Modified brace layout to K&R style where
    required. Wrapped lines as needed to limit line length to 80
    characters. 
    
    Renamed logger to LOGGER. 
    
    Modified log message levels as needed to match logging
    conventions.
    
    Added @Override annotations (commented out as needed) to SPI
    methods. 
    
    Added @SuppressWarnings in places with unchecked cast warnings
    during compilation.
    
    Modified import order to match conventions. 
    
    Added copyright block to source files.
    
    Added version.properties, logging.properties,
    google-enterprise-connector-notes.properties.sample, and
    NotesMain.java.   
    
    Modified build.xml to match other connectors; used Livelink
    connector's build.xml as a template. 
    
    Added TODO messages as requested in code review. 
    
    Removed * imports. 
    
    Renamed NotesConnector config property from IDPassword to
    IdPassword to match coding conventions for property names. 
    
    Code review: http://codereview.appspot.com/4981051/

--- projects/notes-core/source/java/com/google/enterprise/connector/notes/NotesAuthenticationManager.java
+++ projects/notes-core/source/java/com/google/enterprise/connector/notes/NotesAuthenticationManager.java
@@ -1,9 +1,22 @@
+// Copyright 2011 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.google.enterprise.connector.notes;
 
-
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
+import com.google.enterprise.connector.spi.AuthenticationIdentity;
+import com.google.enterprise.connector.spi.AuthenticationManager;
+import com.google.enterprise.connector.spi.AuthenticationResponse;
 import lotus.domino.Database;
 import lotus.domino.Document;
 import lotus.domino.NotesFactory;
@@ -11,105 +24,118 @@ import lotus.domino.NotesThread;
 import lotus.domino.Session;
 import lotus.domino.View;
 
-
-import com.google.enterprise.connector.spi.AuthenticationIdentity;
-import com.google.enterprise.connector.spi.AuthenticationManager;
-import com.google.enterprise.connector.spi.AuthenticationResponse;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 class NotesAuthenticationManager implements AuthenticationManager {
-	private static final String CLASS_NAME = NotesAuthenticationManager.class.getName();
-	private static final Logger _logger = Logger.getLogger(CLASS_NAME);
-	private NotesConnectorSession ncs = null;
-	Session nSession = null;
-	Database namesDb = null;
-	Database acDb = null;
-	View usersVw = null;
-	View peopleVw = null;
-	Document authDoc = null;
-	Document personDoc = null;
-	
-	
-	public NotesAuthenticationManager(NotesConnectorSession session){
-		final String METHOD = "NotesAuthenticationManager";
-		_logger.logp(Level.INFO, CLASS_NAME, METHOD, "NotesAuthenticationManager being created.");
+  private static final String CLASS_NAME = 
+      NotesAuthenticationManager.class.getName();
+  private static final Logger LOGGER = Logger.getLogger(CLASS_NAME);
 
-		ncs = session;
-	}
-	
-	public void recycleDominoObjects () {
-		try {
-			if (null != personDoc)
-				personDoc.recycle();
-			if (null != authDoc)
-				authDoc.recycle();
-			if (null != usersVw)
-				usersVw.recycle();
-			if (null!= peopleVw)
-				peopleVw.recycle();
-			if (null != acDb)
-				acDb.recycle();
-			if (null!= namesDb)
-				namesDb.recycle();
-			if (nSession!= null)
-				nSession.recycle();
-		}
-		catch (Exception e) {
-			_logger.log(Level.SEVERE, CLASS_NAME, e);
-		}
-	}
-	
-	public AuthenticationResponse authenticate(AuthenticationIdentity id) {
-		final String METHOD = "authenticate";
-		_logger.entering(CLASS_NAME, METHOD);
-		
-		try {
-			String pvi = id.getUsername();
-			_logger.logp(Level.INFO, CLASS_NAME, METHOD, "Authenticating user " + pvi);
-			NotesThread.sinitThread();
-			
-			nSession = NotesFactory.createSessionWithFullAccess(ncs.getPassword());
-			namesDb = nSession.getDatabase(ncs.getServer(), "names.nsf");  //TODO: Check what we need to support here
+  private NotesConnectorSession ncs = null;
+  Session nSession = null;
+  Database namesDb = null;
+  Database acDb = null;
+  View usersVw = null;
+  View peopleVw = null;
+  Document authDoc = null;
+  Document personDoc = null;
+        
+  public NotesAuthenticationManager(NotesConnectorSession session) {
+    final String METHOD = "NotesAuthenticationManager";
+    LOGGER.logp(Level.FINEST, CLASS_NAME, METHOD, 
+        "NotesAuthenticationManager being created.");
 
-			acDb = nSession.getDatabase(null, null);
-			_logger.logp(Level.FINER, CLASS_NAME, METHOD, "Opening ACL database " + ncs.getServer() + " : " + ncs.getACLDbReplicaId());
-			acDb.openByReplicaID(ncs.getServer(), ncs.getACLDbReplicaId());
+    ncs = session;
+  }
+        
+  public void recycleDominoObjects() {
+    try {
+      if (null != personDoc) {
+        personDoc.recycle();
+      }
+      if (null != authDoc) {
+        authDoc.recycle();
+      }
+      if (null != usersVw) {
+        usersVw.recycle();
+      }
+      if (null!= peopleVw) {
+        peopleVw.recycle();
+      }
+      if (null != acDb) {
+        acDb.recycle();
+      }
+      if (null!= namesDb) {
+        namesDb.recycle();
+      }
+      if (nSession!= null) {
+        nSession.recycle();
+      }
+    }
+    catch (Exception e) {
+      LOGGER.log(Level.WARNING, CLASS_NAME, e);
+    }
+  }
 
-			peopleVw = acDb.getView(NCCONST.VIEWACPEOPLE);
+  /* @Override */
+  public AuthenticationResponse authenticate(AuthenticationIdentity id) {
+    final String METHOD = "authenticate";
+                
+    try {
+      String pvi = id.getUsername();
+      LOGGER.logp(Level.FINE, CLASS_NAME, METHOD, 
+          "Authenticating user " + pvi);
+      NotesThread.sinitThread();
+      
+      nSession = NotesFactory.createSessionWithFullAccess(ncs.getPassword());
 
-			// Resolve the PVI to their Notes names and groups
-			personDoc = peopleVw.getDocumentByKey(pvi, true);
-			if (null == personDoc) {
-				_logger.logp(Level.WARNING, CLASS_NAME, METHOD, "Person not found in ACL database " + pvi);
-				return new AuthenticationResponse(false, null);
-			}
-			String NotesName = personDoc.getItemValueString(NCCONST.ACITM_NOTESNAME).toLowerCase();
-			_logger.logp(Level.INFO, CLASS_NAME, METHOD, "Authentication user using Notes name " + NotesName);
-			usersVw = namesDb.getView("($Users)");
+      // TODO: Check what we need to support here
+      namesDb = nSession.getDatabase(ncs.getServer(), "names.nsf");  
 
-			// Resolve the PVI to their Notes names and groups
-			authDoc = usersVw.getDocumentByKey(NotesName, true);
-			if (null == authDoc) {
+      acDb = nSession.getDatabase(null, null);
+      LOGGER.logp(Level.FINER, CLASS_NAME, METHOD, "Opening ACL database " + 
+          ncs.getServer() + " : " + ncs.getACLDbReplicaId());
+      acDb.openByReplicaID(ncs.getServer(), ncs.getACLDbReplicaId());
 
-				return new AuthenticationResponse(false, null);
-			}
-			String hashedPassword = authDoc.getItemValueString("HTTPPassword");
-			if (nSession.verifyPassword(id.getPassword(), hashedPassword)) {
-					_logger.logp(Level.INFO, CLASS_NAME, METHOD, "User succesfully authenticated " + NotesName);
-					return new AuthenticationResponse(true,null);
-			}
-			_logger.logp(Level.WARNING, CLASS_NAME, METHOD, "User failed authentication " + NotesName);
-		}
-		catch(Exception e)
-		{
-			_logger.log(Level.SEVERE, CLASS_NAME, e);
-		}
-		finally
-		{
-			recycleDominoObjects();
-			NotesThread.stermThread();
-			_logger.exiting(CLASS_NAME, METHOD);
-		}
-		return new AuthenticationResponse(false, null);
-	}
+      peopleVw = acDb.getView(NCCONST.VIEWACPEOPLE);
+
+      // Resolve the PVI to their Notes names and groups
+      personDoc = peopleVw.getDocumentByKey(pvi, true);
+      if (null == personDoc) {
+        LOGGER.logp(Level.FINE, CLASS_NAME, METHOD, 
+            "Person not found in ACL database " + pvi);
+        return new AuthenticationResponse(false, null);
+      }
+      String NotesName = personDoc.getItemValueString(NCCONST.ACITM_NOTESNAME)
+          .toLowerCase();
+      LOGGER.logp(Level.FINE, CLASS_NAME, METHOD,
+          "Authentication user using Notes name " + NotesName);
+      usersVw = namesDb.getView("($Users)");
+      
+      // Resolve the PVI to their Notes names and groups
+      authDoc = usersVw.getDocumentByKey(NotesName, true);
+      if (null == authDoc) {
+        return new AuthenticationResponse(false, null);
+      }
+      String hashedPassword = authDoc.getItemValueString("HTTPPassword");
+      if (nSession.verifyPassword(id.getPassword(), hashedPassword)) {
+        LOGGER.logp(Level.FINE, CLASS_NAME, METHOD, 
+            "User succesfully authenticated " + NotesName);
+        return new AuthenticationResponse(true, null);
+      }
+      LOGGER.logp(Level.FINE, CLASS_NAME, METHOD,
+          "User failed authentication " + NotesName);
+    } catch (Exception e) {
+      // TODO: what kinds of Notes exceptions can be caught here?
+      // Should we rethrow an exception (RepositoryException?)
+      // rather than just returning false?
+      LOGGER.log(Level.SEVERE, CLASS_NAME, e);
+    } finally {
+      recycleDominoObjects();
+      NotesThread.stermThread();
+    }
+    return new AuthenticationResponse(false, null);
+  }
 }
--- projects/notes-core/source/javatests/com/google/enterprise/connector/notes/NotesConnectorTest.java
+++ projects/notes-core/source/javatests/com/google/enterprise/connector/notes/NotesConnectorTest.java
@@ -1,51 +1,63 @@
+// Copyright 2011 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.google.enterprise.connector.notes;
 
 import com.google.enterprise.connector.notes.NotesConnector;
 import com.google.enterprise.connector.notes.NotesConnectorSession;
 import com.google.enterprise.connector.notes.NotesTraversalManager;
 import com.google.enterprise.connector.spi.Connector;
-import com.google.enterprise.connector.spi.RepositoryLoginException;
 import com.google.enterprise.connector.spi.RepositoryException;
+import com.google.enterprise.connector.spi.RepositoryLoginException;
 import com.google.enterprise.connector.spi.Session;
 import com.google.enterprise.connector.spi.TraversalManager;
-
 import junit.framework.TestCase;
 
 public class NotesConnectorTest extends TestCase {
 
-        protected void setUp() throws Exception {
-        }
+  @Override
+  protected void setUp() throws Exception {
+  }
 
-        /*
-         * Test method for
-         * 'com.google.enterprise.connector.notes.NotesConnectorSession.login()'
-         */
-        public void testLogin() throws RepositoryLoginException, RepositoryException {
-                Connector connector = new NotesConnector();
-                ((NotesConnector) connector).setServer(ncTest.server);
-                ((NotesConnector) connector).setDatabase(ncTest.database);
-                ((NotesConnector) connector).setIDPassword(ncTest.idpassword);
-                Session sess = connector.login();
-                assertNotNull(sess);
-                assertTrue(sess instanceof NotesConnectorSession);
-        }
+  /*
+   * Test method for
+   * 'com.google.enterprise.connector.notes.NotesConnectorSession.login()'
+   */
+  public void testLogin() throws RepositoryLoginException, RepositoryException {
+    Connector connector = new NotesConnector();
+    ((NotesConnector) connector).setServer(ncTest.server);
+    ((NotesConnector) connector).setDatabase(ncTest.database);
+    ((NotesConnector) connector).setIdPassword(ncTest.idpassword);
+    Session sess = connector.login();
+    assertNotNull(sess);
+    assertTrue(sess instanceof NotesConnectorSession);
+  }
 
-        public void testReset() throws RepositoryLoginException, RepositoryException {
-                Connector connector = new NotesConnector();
-                ((NotesConnector) connector).setServer(ncTest.server);
-                ((NotesConnector) connector).setDatabase(ncTest.database);
-                ((NotesConnector) connector).setIDPassword(ncTest.idpassword);
-                Session sess = connector.login();
-				TraversalManager tm = sess.getTraversalManager();
-                assertNotNull(tm);
-                assertTrue(tm instanceof NotesTraversalManager);
-				tm.startTraversal();
-		try {
-		java.lang.Thread.sleep(10000);
-		}
-		catch (Exception e) {}
-        }
-
-
+  public void testReset() throws RepositoryLoginException, RepositoryException {
+    Connector connector = new NotesConnector();
+    ((NotesConnector) connector).setServer(ncTest.server);
+    ((NotesConnector) connector).setDatabase(ncTest.database);
+    ((NotesConnector) connector).setIdPassword(ncTest.idpassword);
+    Session sess = connector.login();
+    TraversalManager tm = sess.getTraversalManager();
+    assertNotNull(tm);
+    assertTrue(tm instanceof NotesTraversalManager);
+    tm.startTraversal();
+    try {
+      java.lang.Thread.sleep(10000);
+    } catch (Exception e) {
+    }
+  }
 }
 
