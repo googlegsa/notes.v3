9536d99d6e04151aad22b92642b4cb6d90db1354
Thomas Nguyen <tdnguyen@google.com>
2013-09-24 20:58:44 +0000

    Lotus Notes connector fails to lookup abbreviated name when group name is in canonical format (b/10212771)
    Changes:
    - Add a helper method to detect canonical name
    - Update NotesName interface to use Notes API to compute abbreviated name
    - Update group deletion logic to convert canonical name to abbreviated name before performing group lookup
    
    https://codereview.appspot.com/13627052/

--- projects/notes-core/source/java/com/google/enterprise/connector/notes/NotesUserGroupManager.java
+++ projects/notes-core/source/java/com/google/enterprise/connector/notes/NotesUserGroupManager.java
@@ -1856,6 +1856,10 @@ class NotesUserGroupManager {
           continue;
         }
         try {
+          if (Util.isCanonical(groupName)) {
+            NotesName notesGroupName = notesSession.createName(groupName);
+            groupName = notesGroupName.getAbbreviated();
+          }
           NotesDocument notesGroupDoc = groupView.getDocumentByKey(groupName);
           if (notesGroupDoc == null) {
             // This group no longer exists.
