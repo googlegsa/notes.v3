c4909d0b071e2718bf786c6b8f4488ef43f59c38
Thomas Nguyen <tdnguyen@google.com>
2013-09-25 23:42:18 +0000

    Copy 3.0.x branch to /branches/dev/tdnguyen/3.0.x

---
+++ tdnguyen/3.0.x/projects/notes-core/source/javatests/com/google/enterprise/connector/notes/NotesDatabasePollerTest.java
@@ -0,0 +1,201 @@
+// Copyright 2012 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.notes;
+
+import com.google.enterprise.connector.notes.NotesConnector;
+import com.google.enterprise.connector.notes.NotesConnectorSession;
+import com.google.enterprise.connector.notes.client.NotesACL;
+import com.google.enterprise.connector.notes.client.NotesACLEntry;
+import com.google.enterprise.connector.notes.client.NotesDatabase;
+import com.google.enterprise.connector.notes.client.NotesDocument;
+import com.google.enterprise.connector.notes.client.NotesItem;
+import com.google.enterprise.connector.notes.client.NotesSession;
+import com.google.enterprise.connector.notes.client.NotesView;
+import com.google.enterprise.connector.notes.client.mock.NotesACLMock;
+import com.google.enterprise.connector.notes.client.mock.NotesACLEntryMock;
+import com.google.enterprise.connector.notes.client.mock.NotesDatabaseMock;
+import com.google.enterprise.connector.notes.client.mock.NotesDocumentMock;
+import com.google.enterprise.connector.notes.client.mock.NotesItemMock;
+import com.google.enterprise.connector.notes.client.mock.SessionFactoryMock;
+import com.google.enterprise.connector.spi.RepositoryException;
+import com.google.enterprise.connector.spi.Session;
+import com.google.enterprise.connector.spi.SimpleTraversalContext;
+
+import junit.framework.TestCase;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+
+public class NotesDatabasePollerTest extends TestCase {
+
+  static class DatabasePollerTestable extends NotesDatabasePoller {
+
+    boolean calledUpdateGsaPolicyAcl;
+
+    DatabasePollerTestable(NotesConnectorSession session) {
+      super(session);
+    }
+
+    @Override
+    boolean updateGsaPolicyAcl(NotesSession session,
+        NotesDatabase connectorDatabase, NotesDocument dbdoc,
+        Collection<String> permitUsers, Collection<String> permitGroups)
+        throws RepositoryException {
+      calledUpdateGsaPolicyAcl = true;
+      return true;
+    }
+  }
+
+  private static NotesACLMock acl;
+
+  static {
+    acl = new NotesACLMock();
+    acl.addAclEntry(new NotesACLEntryMock("-Default-",
+        NotesACLEntry.TYPE_UNSPECIFIED, NotesACL.LEVEL_NOACCESS));
+    acl.addAclEntry(new NotesACLEntryMock("allowed user1",
+        NotesACLEntry.TYPE_PERSON, NotesACL.LEVEL_READER));
+    acl.addAclEntry(new NotesACLEntryMock("allowed user2",
+        NotesACLEntry.TYPE_PERSON, NotesACL.LEVEL_READER));
+    acl.addAclEntry(new NotesACLEntryMock("denied user1",
+        NotesACLEntry.TYPE_PERSON, NotesACL.LEVEL_NOACCESS));
+    acl.addAclEntry(new NotesACLEntryMock("allowed group1",
+        NotesACLEntry.TYPE_PERSON_GROUP, NotesACL.LEVEL_READER));
+    acl.addAclEntry(new NotesACLEntryMock("allowed group2",
+        NotesACLEntry.TYPE_PERSON_GROUP, NotesACL.LEVEL_READER));
+    acl.addAclEntry(new NotesACLEntryMock("denied group1",
+        NotesACLEntry.TYPE_PERSON_GROUP, NotesACL.LEVEL_NOACCESS));
+  }
+
+  private NotesConnector connector;
+  private SessionFactoryMock factory;
+  private NotesConnectorSession connectorSession;
+  private DatabasePollerTestable poller;
+  private boolean supportsInheritedAcls;
+
+  public NotesDatabasePollerTest() {
+    super();
+  }
+
+  protected void setUp() throws Exception {
+    super.setUp();
+    // TODO: handle both versions of acl support within the tests
+    // and avoid manual property editing.
+    supportsInheritedAcls =
+        Boolean.getBoolean("javatest.supportsinheritedacls");
+    connector = NotesConnectorTest.getConnector();
+    factory = (SessionFactoryMock) connector.getSessionFactory();
+    NotesConnectorSessionTest.configureFactoryForSession(factory);
+    connectorSession =
+        (NotesConnectorSession) connector.login();
+    SimpleTraversalContext context = new SimpleTraversalContext();
+    context.setSupportsInheritedAcls(supportsInheritedAcls);
+    ((NotesTraversalManager) connectorSession.getTraversalManager())
+        .setTraversalContext(context);
+    poller = new DatabasePollerTestable(connectorSession);
+  }
+
+  protected void tearDown() {
+    if (null != connector) {
+      connector.shutdown();
+    }
+  }
+
+  public void testPermitDeny() throws Exception {
+    ArrayList<String> permitUsers = new ArrayList<String>();
+    ArrayList<String> permitGroups = new ArrayList<String>();
+    ArrayList<String> noAccessUsers = new ArrayList<String>();
+    ArrayList<String> noAccessGroups = new ArrayList<String>();
+
+    poller.getPermitDeny(NotesDatabasePollerTest.acl, permitUsers, permitGroups,
+        noAccessUsers, noAccessGroups, connectorSession.createNotesSession());
+    assertEquals(2, permitUsers.size());
+    assertEquals("allowed user1", permitUsers.get(0));
+    assertEquals("allowed user2", permitUsers.get(1));
+    assertEquals(2, noAccessUsers.size());
+    assertEquals("denied user1", noAccessUsers.get(1));
+    assertEquals(2, permitGroups.size());
+    assertEquals("allowed group1", permitGroups.get(0));
+    assertEquals("allowed group2", permitGroups.get(1));
+    assertEquals("denied groups is not zero", 0, noAccessGroups.size());
+  }
+
+  public void testProcessAclCreateDbAclCrawlDoc() throws Exception {
+    // Set up a source database with an ACL and a database
+    // document for that source database.
+    NotesDatabaseMock sourceDatabase = new NotesDatabaseMock("testserver",
+        "sourcedatabase.nsf");
+    sourceDatabase.setACLActivityLog("today");
+    sourceDatabase.setACL(NotesDatabasePollerTest.acl);
+    factory.addDatabase(sourceDatabase);
+    NotesDocumentMock databaseDocument = new NotesDocumentMock();
+    databaseDocument.addItem(new NotesItemMock("name", NCCONST.DITM_ACLTEXT,
+        "type", NotesItem.TEXT, "values", "yesterday"));
+    databaseDocument.addItem(new NotesItemMock("name", NCCONST.DITM_AUTHTYPE,
+        "type", NotesItem.TEXT, "values", NCCONST.AUTH_ACL));
+    databaseDocument.addItem(new NotesItemMock("name", NCCONST.DITM_REPLICAID,
+        "type", NotesItem.TEXT, "values", "replica_id_16chr"));
+    databaseDocument.addItem(new NotesItemMock("name", NCCONST.DITM_SERVER,
+        "type", NotesItem.TEXT, "values", connector.getServer()));
+    databaseDocument.addItem(new NotesItemMock("name", NCCONST.DITM_DOMAIN,
+        "type", NotesItem.TEXT, "values",
+        connectorSession.getDomain(connector.getServer())));
+
+    NotesSession session = connectorSession.createNotesSession();
+    NotesDatabaseMock connectorDatabase =
+        (NotesDatabaseMock) session.getDatabase(
+        connectorSession.getServer(), connectorSession.getDatabase());
+
+    /*
+    // Add the test groups to the group cache.
+    NotesDocumentMock connectorGroup = new NotesDocumentMock();
+    connectorGroup.addItem(new NotesItemMock("name", NCCONST.GCITM_GROUPNAME,
+            "type", NotesItem.TEXT, "values", "allowed group2"));
+    connectorDatabase.addDocument(connectorGroup, NCCONST.VIEWGROUPCACHE);
+    */
+
+    assertFalse(databaseDocument.hasItem(NCCONST.NCITM_DBPERMITUSERS));
+    assertFalse(databaseDocument.hasItem(NCCONST.NCITM_DBPERMITGROUPS));
+    assertFalse(databaseDocument.hasItem(NCCONST.NCITM_DBNOACCESSUSERS));
+    assertFalse(databaseDocument.hasItem(NCCONST.NCITM_DBNOACCESSGROUPS));
+    assertNull(connectorDatabase.getDocumentByUNID("replica_id_16chr"));
+
+    poller.processACL(session, connectorDatabase,
+        sourceDatabase, databaseDocument);
+
+    assertTrue(databaseDocument.hasItem(NCCONST.NCITM_DBPERMITUSERS));
+    assertTrue(databaseDocument.hasItem(NCCONST.NCITM_DBPERMITGROUPS));
+    assertTrue(databaseDocument.hasItem(NCCONST.NCITM_DBNOACCESSUSERS));
+    assertTrue(databaseDocument.hasItem(NCCONST.NCITM_DBNOACCESSGROUPS));
+
+    NotesDocument aclCrawlDoc =
+        connectorDatabase.getDocumentByUNID("replica_id_16chr");
+    if (supportsInheritedAcls) {
+      assertNotNull(aclCrawlDoc);
+      assertEquals("true",
+          aclCrawlDoc.getItemValueString(NCCONST.NCITM_DBACL));
+      assertEquals("http://testserver.testdomain/replica_id_16chr/"
+          + NCCONST.DB_ACL_INHERIT_TYPE_ANDBOTH,
+          aclCrawlDoc.getItemValueString(NCCONST.ITM_DOCID));
+    } else {
+      assertTrue(poller.calledUpdateGsaPolicyAcl);
+      assertNull(aclCrawlDoc);
+    }
+  }
+}
---
+++ tdnguyen/3.0.x/projects/notes-core/source/javatests/com/google/enterprise/connector/notes/client/mock/NotesItemMock.java
@@ -0,0 +1,191 @@
+// Copyright 2011 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.notes.client.mock;
+
+import com.google.enterprise.connector.notes.client.NotesDateTime;
+import com.google.enterprise.connector.notes.client.NotesItem;
+import com.google.enterprise.connector.spi.RepositoryException;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Vector;
+import java.util.logging.Logger;
+
+public class NotesItemMock extends NotesBaseMock implements NotesItem {
+  private static final String CLASS_NAME = NotesItemMock.class.getName();
+
+  /** The logger for this class. */
+  private static final Logger LOGGER =
+      Logger.getLogger(CLASS_NAME);
+
+  private Map<String, Object> properties = new HashMap<String, Object>();
+  private boolean isReaders = false;
+  private boolean isAuthors = false;
+
+  public NotesItemMock(Object... args) {
+    int valuesIndex = -1;
+    for (int i = 0; i < args.length; i = i + 2) {
+      String name = args[i].toString();
+      if ("values".equals(name)) {
+        valuesIndex = i + 1;
+        break;
+      }
+      properties.put(name, args[i + 1]);
+    }
+    if (valuesIndex != -1 && valuesIndex < args.length) {
+      Vector<Object> values = new Vector<Object>();
+      for (int i = valuesIndex; i < args.length; i++) {
+        if (null == args[i]) {
+          continue;
+        }
+        if (args[i] instanceof Vector) {
+          for (Object o : (Vector) args[i]) {
+            values.add(o);
+          }
+        } else if (args[i] instanceof Object[]) {
+          for (Object o : (Object[]) args[i]) {
+            values.add(o);
+          }
+        } else {
+          values.add(args[i]);
+        }
+      }
+      properties.put("values", values);
+      LOGGER.finest("NotesItemMock<init> values: " + properties.get("values"));
+    }
+  }
+
+  /** {@inheritDoc} */
+  /* @Override */
+  @SuppressWarnings("unchecked")
+  public NotesDateTime getDateTimeValue() throws RepositoryException {
+    LOGGER.entering(CLASS_NAME, "getDateTimeValue");
+    Vector<Object> values = (Vector<Object>)properties.get("values");
+    for (Object o : values) {
+      if (o instanceof NotesDateTime) {
+        return (NotesDateTime)o;
+      }
+    }
+    return null;
+  }
+  
+  /** {@inheritDoc} */
+  /* @Override */
+  public String getName() throws RepositoryException {
+    LOGGER.entering(CLASS_NAME, "getName");
+    return (String) properties.get("name");
+  }
+
+  /** {@inheritDoc} */
+  /* @Override */
+  public int getType() throws RepositoryException {
+    LOGGER.entering(CLASS_NAME, "getType");
+    Object type = properties.get("type");
+    if (null == type) {
+      return -1;
+    }
+    return (Integer) type;
+  }
+
+  /** {@inheritDoc} */
+  /* @Override */
+  public String getText(int maxlen) throws RepositoryException {
+    LOGGER.entering(CLASS_NAME, "getText");
+    Vector values = getValues();
+    if (values == null) {
+      return null;
+    }
+    StringBuilder builder = new StringBuilder();
+    for (Object o : values) {
+      builder.append(o.toString()).append(";");
+    }
+    builder.deleteCharAt(builder.length() -1);
+    return builder.toString();
+  }
+
+  /** {@inheritDoc} */
+  /* @Override */
+  public boolean isReaders() throws RepositoryException {
+    LOGGER.entering(CLASS_NAME, "isReaders");
+    return isReaders;
+  }
+
+  public void setReaders(boolean readers) {
+    isReaders = readers;
+  }
+
+  /** {@inheritDoc} */
+  /* @Override */
+  public boolean isAuthors() throws RepositoryException {
+    LOGGER.entering(CLASS_NAME, "isAuthors");
+    return isAuthors;
+  }
+
+  public void setAuthors(boolean authors) {
+    isAuthors = authors;
+  }
+
+  /** {@inheritDoc} */
+  /* @Override */
+  public Vector getValues() throws RepositoryException {
+    LOGGER.entering(CLASS_NAME, "getValues");
+    return (Vector) properties.get("values");
+  }
+
+  /** {@inheritDoc} */
+  /* @Override */
+  public void appendToTextList(String value) throws RepositoryException {
+    LOGGER.entering(CLASS_NAME, "appendToTextList");
+    @SuppressWarnings("unchecked")
+        Vector<String> values = (Vector<String>) getValues();
+    if (values == null) {
+      Vector<String> v = new Vector<String>();
+      v.add(value);
+      properties.put("values", v);
+    } else {
+      values.add(value);
+    }
+  }
+
+  /** {@inheritDoc} */
+  /* @Override */
+  @SuppressWarnings("unchecked")
+  public void appendToTextList(Vector values) throws RepositoryException {
+    LOGGER.entering(CLASS_NAME, "appendToTextList");
+    Vector currentValues = getValues();
+    if (currentValues == null) {
+      Vector v = new Vector();
+      v.addAll(values);
+      properties.put("values", v);
+    } else {
+      currentValues.addAll(values);
+    }
+  }
+
+  /** {@inheritDoc} */
+  /* @Override */
+  public void setSummary(boolean summary) throws RepositoryException {
+    LOGGER.entering(CLASS_NAME, "setSummary");
+  }
+
+  public String toString() {
+    try {
+      return getName() + " = " + getText(256);
+    } catch (RepositoryException e) {
+      return "";
+    }
+  }
+}
